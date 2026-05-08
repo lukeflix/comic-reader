41a6e282c7489c95045d894f7485fa5efa348543
lukeflix <lukeflixstreamimg@gmail.com>
2026-05-08 07:43:23 -0500

    Soporte CBR real con junrar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    packaging {
+        resources {
+            excludes += setOf("META-INF/DEPENDENCIES", "META-INF/LICENSE", "META-INF/NOTICE")
+        }
+    }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.github.junrar:junrar:7.5.4")
 }
